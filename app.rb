21c059e3dc807a6821768ac5041509f4dd1748d8
Juri F <friindel@mail.ru>
2020-05-26 16:59:04 +0100

    lesson 29

--- app.rb
+++ app.rb
@@ -12,6 +12,44 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Contact < ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+  # user_name, email, date_stamp, barber
+  @username = params[:username]
+  @datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+	@phone = params[:phone]
+
+
+
+  erb "<h2>Thank You for the visit</h2>"
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+
+
+	erb "<h2>Thank You. We will deal with your message ASAP</h2>"
 end
